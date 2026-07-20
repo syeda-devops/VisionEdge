73fa544c9a94510c27813adbd8307fa427e2820f
Sadiya Hashmi <syedahashmi1118@gmail.com>
2026-07-20 09:40:19 +0530

    feat: integrate FrameProvider with Detector

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from backend.core.config import MODEL_PATH, IMAGE_PATH
+from backend.detector.detector import Detector
+from backend.streaming.frame_provider import FrameProvider
+
+
 def main():
-    print("VisionEdge initialized.")
+
+    provider = FrameProvider(IMAGE_PATH)
+
+    detector = Detector(MODEL_PATH)
+
+    frame = provider.get_frame()
+
+    detections = detector.predict(frame)
+
+
+    print(detections)
 
 
 if __name__ == "__main__":
