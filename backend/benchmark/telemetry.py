b241c437876322b77ddc8e56e2a67d923a43ccc4
DrakshayaniShivarathri <shivarathridrakshayani@gmail.com>
2026-08-10 23:39:13 +0530

    Revert "fix: improve telemetry stream lifecycle logging"
    This reverts commit 7ec48c7ad83228a041c22a9ab8dcc4f4cf2af4f1.

--- backend/benchmark/telemetry.py
+++ backend/benchmark/telemetry.py
@@ -38,16 +38,11 @@ class TelemetryHub:
         log.info("Registered stream: %s", stream_id)
 
     def unregister_stream(self, stream_id: str):
-        if self._streams.pop(stream_id, None) is not None:
-            log.info("Unregistered stream: %s", stream_id)
-        else:
-            log.warning("Attempted to unregister unknown stream: %s", stream_id)
+        self._streams.pop(stream_id, None)
 
     def tick(self, stream_id: str):
         if stream_id in self._streams:
             self._streams[stream_id].tick()
-        else:
-            log.warning("Telemetry tick received for unknown stream: %s", stream_id)
 
     def gpu_stats(self) -> dict:
         """
